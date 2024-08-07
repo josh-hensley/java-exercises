0339cc16bbdd3b357e13e27ab9bb5b677d286bf7
josh-hensley <me@joshhensley.com>
2024-08-07 15:06:07 -0500

    finished hello world, finished step one of Syntax, Types, Variables

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+    }
+}
