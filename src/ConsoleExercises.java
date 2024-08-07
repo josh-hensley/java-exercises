2843f49c73426cc18d5fcd57aa8e6f45a63e43f0
josh-hensley <me@joshhensley.com>
2024-08-07 16:31:16 -0500

    cleaned up folder with skipped steps taken

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args){
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %s%n", pi);
+        System.out.println("Input an integer: ");
+        int userInt = scanner.nextInt();
+        System.out.format("Your integer is %s", userInt);
+        String userWord1 = scanner.nextLine();
+        String userWord2 = scanner.nextLine();
+        String userWord3 = scanner.nextLine();
+        System.out.println("%s , %s, %s.", userWord1, userWord2, userWord3);
+    }
+}
